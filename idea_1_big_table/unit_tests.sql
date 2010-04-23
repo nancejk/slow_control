456031ea5cce0ec2db825e91bee696004f67707a
nancejk <nancejk@apprentice.(none)>
2010-04-23 16:51:23 -0700

    Fixed syntax in files, all now work correctly.

--- idea_1_big_table/unit_tests.sql
+++ idea_1_big_table/unit_tests.sql
@@ -1,11 +1,11 @@
-# Try to fill the table channel_names
-insert into channel_names (chan_id,hnames) values (1,'Test Channel 1');
-insert into channel_names (chan_id,hnames) values (2,'Test Channel 2');
-insert into channel_names (chan_id,hnames) values (3,'Test Channel 3');
-insert into channel_names (chan_id,hnames) values (4,'Test Channel 4');
-insert into channel_names (chan_id,hnames) values (5,'Test Channel 5');
-insert into channel_names (chan_id,hnames) values (6,'Test Channel 6');
-insert into channel_names (chan_id,hnames) values (7,'Test Channel 7');
-insert into channel_names (chan_id,hnames) values (8,'Test Channel 8');
-insert into channel_names (chan_id,hnames) values (9,'Test Channel 9');
-insert into channel_names (chan_id,hnames) values (10,'Test Channel 10');
+-- Try to fill the table channel_names
+insert into channel_names (chan_id,hname) values (1,'Test Channel 1');
+insert into channel_names (chan_id,hname) values (2,'Test Channel 2');
+insert into channel_names (chan_id,hname) values (3,'Test Channel 3');
+insert into channel_names (chan_id,hname) values (4,'Test Channel 4');
+insert into channel_names (chan_id,hname) values (5,'Test Channel 5');
+insert into channel_names (chan_id,hname) values (6,'Test Channel 6');
+insert into channel_names (chan_id,hname) values (7,'Test Channel 7');
+insert into channel_names (chan_id,hname) values (8,'Test Channel 8');
+insert into channel_names (chan_id,hname) values (9,'Test Channel 9');
+insert into channel_names (chan_id,hname) values (10,'Test Channel 10');
