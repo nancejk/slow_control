832edf6599ac40df292a6f9fca7dbeef6a76f95b
nancejk <nancejk@apprentice.(none)>
2010-04-23 17:50:56 -0700

    Added first benchmark to unit tests - return all rows where value is outside spec

--- idea_1_big_table/unit_tests.sql
+++ idea_1_big_table/unit_tests.sql
@@ -1,11 +1,4 @@
--- Try to fill the table channel_names
-insert into channel_names (chan_id,hname) values (1,'Test Channel 1');
-insert into channel_names (chan_id,hname) values (2,'Test Channel 2');
-insert into channel_names (chan_id,hname) values (3,'Test Channel 3');
-insert into channel_names (chan_id,hname) values (4,'Test Channel 4');
-insert into channel_names (chan_id,hname) values (5,'Test Channel 5');
-insert into channel_names (chan_id,hname) values (6,'Test Channel 6');
-insert into channel_names (chan_id,hname) values (7,'Test Channel 7');
-insert into channel_names (chan_id,hname) values (8,'Test Channel 8');
-insert into channel_names (chan_id,hname) values (9,'Test Channel 9');
-insert into channel_names (chan_id,hname) values (10,'Test Channel 10');
+-- All rows where the value is not within limits.
+select('Grabbing all rows where the value is outside thresholds');
+explain analyze select meas_id,data.chan_id,dt,value,min_value,max_value from data inner join channel_limits on data.chan_id=channel_limits.chan_id where (data.value < channel_limits.min_value or data.value > channel_limits.max_value);
+
