0517286909485f7cddc076770f5b5ac7a90fc06f
nancejk <nancejk@apprentice.(none)>
2010-04-23 20:18:38 -0700

    Added another function.

--- idea_1_big_table/unit_tests.sql
+++ idea_1_big_table/unit_tests.sql
@@ -2,3 +2,6 @@
 select('Grabbing all rows where the value is outside thresholds');
 explain analyze select meas_id,data.chan_id,dt,value,min_value,max_value from data inner join channel_limits on data.chan_id=channel_limits.chan_id where (data.value < channel_limits.min_value or data.value > channel_limits.max_value);
 
+-- Grab all data that was ever on a tuesday.
+select('Tuesday data');
+select meas_id,chan_id,value from data where extract(dow from dt)=3;
