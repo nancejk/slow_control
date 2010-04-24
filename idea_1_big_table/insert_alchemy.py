f6de6371a492160204b42831bd705ee281187e0d
nancejk <nancejk@apprentice.(none)>
2010-04-23 20:02:37 -0700

    Turned verbose off, as everything is working fine now.
    The argument is the power of 10 which will multiply 1000 to give the final
    number of records pushed.

--- idea_1_big_table/insert_alchemy.py
+++ idea_1_big_table/insert_alchemy.py
@@ -13,7 +13,7 @@ td_base = datetime(year=2010,month=1,day=1)
 
 # create metadata object, bind to connection.  Create session.
 metadata = MetaData()
-engine = create_engine('postgres://scuser@127.0.0.1/big_table', echo=True)
+engine = create_engine('postgres://scuser@127.0.0.1/big_table', echo=False)
 metadata.bind = engine
 
 # table description
@@ -23,6 +23,6 @@ numpush = 0
 i = data_table.insert()
                   
 # Stick 10^4 records in the database using my favorite list comprehension
-for iteration in range(10**sys.argv[1]):
+for iteration in range(1*10**int(sys.argv[1])):
   tlist = [{'value':uniform(-10,10),'chan_id':randint(1,10),'dt':td_base + timedelta(days=5*uniform(0,365))} for placeholder in range(1000)]
   i.execute(tlist)
